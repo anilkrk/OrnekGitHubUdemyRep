644abe8f4c23714ba19963b4b482c9792b53e200
anlkrk <anilkarak62@gmail.com>
2024-08-01 23:05:41 +0300

    Changing somethings
    Degisiklikler yapildi

---
+++ UdemyGTHub/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OrnekGitHubUdemy.Controllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+        void Start()
+        {
+            // Player Controller Reference'lari Cache'ler
+        }
+
+        void Update()
+        {
+            // Player Controller Reference'lari Cachelendigi degiskenleri kullanilir.
+        }
+    }
+}
